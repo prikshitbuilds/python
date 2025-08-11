57d4105b959a75cfed1f87cb4baa6d46508a9f48
prikshitbuilds <prikshitbuilds@gmail.com>
2025-08-11 08:28:57 +0530

    Fruit ripeness prob

---
+++ 10_Conditional+problems/04_fruit_ripeness_checker.py
@@ -0,0 +1,9 @@
+fruit=input("Enter Fruit name")
+color=input("Enter Fruit Color")
+
+if color == "Green":
+    print(fruit,"is Unripe")
+elif color == "Yellow":
+    print(fruit,"is Ripe")
+else:
+    print(fruit,"is overipe")
